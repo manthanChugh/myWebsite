b9e9f7b322e2f09379ffd36508d6b36ce7fb3c05
manthanChugh <85697142+manthanChugh@users.noreply.github.com>
2025-01-25 12:28:48 +0000

    first commit

---
+++ myWebsite/src/pages/Contact.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import pic from "C:/Manthan/Coding/React Projects/react-project/src/images/manthan.jpg";
+import './pages.css'
+import Navbar from 'C:/Manthan/Coding/React Projects/react-project/src/Navbar.jsx';
+
+function Main(){
+    return(        
+        <main>
+            <h1>Contact Me</h1>
+        </main>
+    );
+}
+
+function Contact() {
+    return(
+        <div>
+            <Main/>
+        </div> )
+}
+export default Contact
